81e7dc384d4c86f39557ed49d7b311f9b6cb28ac
Csrhdz <105759540+Csrhdz@users.noreply.github.com>
2024-12-18 15:09:30 +0000

    actualizar

--- js/sw-register.js
+++ js/sw-register.js
@@ -1,12 +1,10 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/Evidencia_Instalaciones/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registrado con éxito:', registration);
-      })
-      .catch((error) => {
-        console.log('Error al registrar el Service Worker:', error);
-      });
-  });
+  navigator.serviceWorker
+    .register('/Evidencia_Instalaciones/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registrado con éxito:', registration);
+    })
+    .catch((error) => {
+      console.error('Error al registrar el Service Worker:', error);
+    });
 }
-
